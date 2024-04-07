3eb9b6b1ce36140fdd2e221f66118193339aa645
Arnold Chand <creativenull@outlook.com>
2024-04-07 16:23:49 -0400

    feat(init): WebRun

--- lua/web/init.lua
+++ lua/web/init.lua
@@ -1,6 +1,7 @@
 local lsp_shared = require("web.lsp._shared")
 local event = require("web.event")
 local validator = require("web.validator")
+local utils = require("web.utils")
 local M = {}
 
 local default_setup_opts = {
@@ -61,8 +62,36 @@ function M.setup(setup_opts)
 			lsp_shared.register_common_user_commands(ev.buf)
 		end,
 	})
+
+	vim.api.nvim_create_user_command("WebRun", function(cmd)
+		local script = cmd.fargs[1]
+		local pm = utils.fs.get_package_manager()
+
+		if pm == "npm" then
+			vim.cmd(string.format("terminal npm run %s", script))
+		elseif pm == "yarn" then
+			vim.cmd(string.format("terminal yarn %s", script))
+		elseif pm == "pnpm" then
+			vim.cmd(string.format("terminal pnpm %s", script))
+		end
+	end, {
+		nargs = 1,
+		complete = function()
+			local packagejson_filepath = string.format("%s/package.json", vim.loop.cwd())
+			if vim.fn.filereadable(packagejson_filepath) == 0 then
+				return {}
+			end
+
+			local json = vim.json.decode(utils.fs.readfile(packagejson_filepath))
+			if vim.tbl_isempty(json.scripts) then
+				return {}
+			end
+
+			return vim.tbl_keys(json.scripts)
+		end,
+	})
 end
 
-M.format = require('web.format').handle
+M.format = require("web.format").handle
 
 return M
