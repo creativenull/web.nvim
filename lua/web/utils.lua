4fd4ba8012a0eb9678325334746bafa922f99583
Arnold Chand <creativenull@outlook.com>
2024-04-07 16:23:01 -0400

    feat(utils): read file contents, get package mgr

--- lua/web/utils.lua
+++ lua/web/utils.lua
@@ -11,10 +11,33 @@ function M.fs.find_nearest(list)
 	return vim.fs.dirname(vim.fs.find(list, { upward = true })[1])
 end
 
+function M.fs.readfile(filepath)
+	local fd = vim.loop.fs_open(filepath, "r", 438)
+	local stat = vim.loop.fs_fstat(fd)
+	local contents = vim.loop.fs_read(fd, stat.size)
+	vim.loop.fs_close(fd)
+
+	return contents
+end
+
+function M.fs.get_package_manager()
+	local yarnlock = string.format("%s/yarn.lock", vim.loop.cwd())
+	if vim.fn.filereadable(yarnlock) == 1 then
+		return "yarn"
+	end
+
+	local pnpmlock = string.format("%s/pnpm-lock.yaml", vim.loop.cwd())
+	if vim.fn.filereadable(pnpmlock) == 1 then
+		return "pnpm"
+	end
+
+	return "npm"
+end
+
 M.err = {}
 
 function M.err.writeln(msg)
-  vim.api.nvim_notify(string.format("[web.nvim] %s", msg), vim.log.levels.WARN, {})
+	vim.api.nvim_notify(string.format("[web.nvim] %s", msg), vim.log.levels.WARN, {})
 end
 
 return M
