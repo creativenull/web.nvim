914f8dbdac1cd179a6c7fda18fd05870df51d047
Arnold Chand <creativenull@outlook.com>
2025-07-19 23:08:36 -0400

    feat(vue): add tsserver/request and response

--- lua/web/lsp/vue.lua
+++ lua/web/lsp/vue.lua
@@ -34,6 +34,29 @@ local function _config(vue_options, user_options)
       on_attach = user_options.on_attach,
       capabilities = user_options.capabilities,
       root_dir = utils.fs.find_nearest(M.root_dirs),
+      on_init = function(client)
+        -- Shamelessly copied from: https://github.com/vuejs/language-tools/wiki/Neovim/1d846ecf8f2018ffc7ce1c0a62645a09d5d3c156
+        client.handlers["tsserver/request"] = function(_, result, context)
+          local clients = vim.lsp.get_clients({ bufnr = context.bufnr, name = "vtsls" })
+          if #clients == 0 then
+            vim.notify("Could not find `vtsls` lsp client, `vue_ls` would not work without it.", vim.log.levels.ERROR)
+            return
+          end
+          local ts_client = clients[1]
+
+          local param = unpack(result)
+          local id, command, payload = unpack(param)
+          ts_client:exec_cmd({
+            title = "vue_request_forward", -- You can give title anything as it's used to represent a command in the UI, `:h Client:exec_cmd`
+            command = "typescript.tsserverRequest",
+            arguments = { command, payload },
+          }, { bufnr = context.bufnr }, function(_, r)
+            local response_data = { { id, r.body } }
+            ---@diagnostic disable-next-line: param-type-mismatch
+            client:notify("tsserver/response", response_data)
+          end)
+        end
+      end,
     }
   end
 
