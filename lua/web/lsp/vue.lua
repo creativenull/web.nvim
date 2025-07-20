3494cf790d848bc84c5c7401e4ceb21680e39d5b
Arnold Chand <creativenull@outlook.com>
2025-07-19 23:24:30 -0400

    fix(vue): update on_init callback

--- lua/web/lsp/vue.lua
+++ lua/web/lsp/vue.lua
@@ -38,23 +38,24 @@ local function _config(vue_options, user_options)
         -- Shamelessly copied from: https://github.com/vuejs/language-tools/wiki/Neovim/1d846ecf8f2018ffc7ce1c0a62645a09d5d3c156
         client.handlers["tsserver/request"] = function(_, result, context)
           local clients = vim.lsp.get_clients({ bufnr = context.bufnr, name = "vtsls" })
+
           if #clients == 0 then
-            vim.notify("Could not find `vtsls` lsp client, `vue_ls` would not work without it.", vim.log.levels.ERROR)
+            utils.warn("Could not find `vtsls` lsp client, `vue_ls` would not work without it.")
+
             return
           end
-          local ts_client = clients[1]
 
+          local ts_client = clients[1]
           local param = unpack(result)
           local id, command, payload = unpack(param)
-          ts_client:exec_cmd({
-            title = "vue_request_forward", -- You can give title anything as it's used to represent a command in the UI, `:h Client:exec_cmd`
-            command = "typescript.tsserverRequest",
-            arguments = { command, payload },
-          }, { bufnr = context.bufnr }, function(_, r)
-            local response_data = { { id, r.body } }
-            ---@diagnostic disable-next-line: param-type-mismatch
-            client:notify("tsserver/response", response_data)
-          end)
+
+          ts_client:exec_cmd(
+            { command = "typescript.tsserverRequest", arguments = { command, payload } },
+            { bufnr = context.bufnr },
+            function(_, r)
+              client:notify("tsserver/response", { { id, r.body } })
+            end
+          )
         end
       end,
     }
